72745b9edf409c8d11c03aebdb0a66b163523c9a
Daniel Alejandro Escobar Prieto <daescobarp@unal.edu.co>
2020-07-01 17:20:59 -0500

    added modal

---
+++ src/Components/Forum/ForumModal.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Modal, Form, Input } from "antd";
+export default (props) => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Modal visible={true}>
+      <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <Form.Item label="Title">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Body">
+          <Input.TextArea />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
